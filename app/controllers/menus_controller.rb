5e6ce8b1a9cb998252f96d62bd752b92e60f84cc
Jake <jdai54@gmail.com>
2018-02-01 15:49:51 -0800

    refactored menu view and items

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,21 @@
 class MenusController < ApplicationController
   def show
     @items = Item.all
+    @appetizers = Item.where(category: "Appetizers")
+    @soups = Item.where(category: "Soups")
+    @seafood = Item.where(category: "Seafood")
+    @lamb = Item.where(category: "Lamb")
+    @beef = Item.where(category: "Beef")
+    @poultry = Item.where(category: "Poultry")
+    @vegetables = Item.where(category: "Vegetables")
+    @soft_noodles = Item.where(category: "Soft Noodles")
+    @fried_rice = Item.where(category: "Fried Rice")
+    @egg_foo_young = Item.where(category: "Egg Foo Young")
+    @chop_suey = Item.where(category: "Chop Suey")
+    @chow_mein = Item.where(category: "Chow Mein")
+    @rice_bowls = Item.where(category: "Rice Bowls")
+
+
+
   end
 end
