d1b8ef2bfe2e35cc83bd688fbcd7a483fdf2f576
Jake <jdai54@gmail.com>
2018-05-05 01:40:10 -0700

    Changed welcome/index route and added padding to about image

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :reservations
   resource :menu, only: [:show]
   resources :orders
-  
+
   devise_for :users
-  get "welcome/index"
   get "welcome/about"
   get "welcome/contact"
 
